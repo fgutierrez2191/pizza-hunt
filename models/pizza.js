cee015ce6672a4042a7b44e8a688f04c27f6e433
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-02-08 19:24:05 -0500

    comment.create is not a function error

--- models/pizza.js
+++ models/pizza.js
@@ -1,21 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
         type: String
-    },
-    createdBy: {
+      },
+      createdBy: {
         type: String
-    },
-    createdAt: {
+      },
+      createdAt: {
         type: Date,
-        default: Date.now
-    },
-    size: {
+        default: Date.now,
+      },
+      size: {
         type: String,
         default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
     },
-    toppings: []
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
+
+//get total count of comment and replies on retrieval
+PizzaSchema.virtual('CommentCount').get(function() {
+    return this.comments.length;
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,4 @@
-// const router = require('express').Router();
-// const htmlRoutes = require('./html/html-routes');
 
-// router.use('/', htmlRoutes);
-
-// router.use((req, res) => {
-//   res.status(404).send('<h1>😝 404 Error!</h1>');
-// });
-
-// module.exports = router;
 
 const router = require('express').Router();
 // Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
